b8465c1f9f83ec0896e9aa9df7f0107c1b697c5d
Quddus1916 <quddusjunior1916@gmail.com>
2021-05-06 02:16:51 -0500

    intro

---
+++ globalvariables.js
@@ -0,0 +1,14 @@
+
+// _dirname = path to current directory
+
+const currentdirectory = _dirname;
+console.log(currentdirectory);
+
+// _filename  = file name
+console.log(_filename);
+
+// require = function to use modules
+const fs = require("fs");
+// module = info about current module
+
+console.log(module);
